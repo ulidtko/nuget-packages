320c51d81e4d978d8c88ff9e564ffc5cca9b8183
Max ulidtko <ulidtko@gmail.com>
2013-08-02 19:45:37 +0300

    initial commit

---
+++ sublime/tools/chocolateyInstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageName = 'sublime'
+$installerType = 'exe'
+$url = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20Setup.exe'
+$url64 = 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2%20x64%20Setup.exe'
+$silentArgs = '/silent'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" "$url64" -validExitCodes $validExitCodes
